33a99c7e732b340dc12182344902cb052d6b0754
1900365 <1900365@qq.com>
2016-08-04 23:07:21 +0800

    task 2_2 : struct init

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,8 +7,16 @@ struct student {
     var mathScore:Int
     var englishScore:Int
     var averageScore:Double
-    
-    init (name: String, mathScore: Int, englishScore: Int, averageScore: Double) {
-        self.name
+
+    init (name:String="No Name", mathScore:Int=0, englishScore:Int=0, averageScore:Double=0) {
+        self.name = name
+        self.mathScore = mathScore
+        self.englishScore = englishScore
+        self.averageScore = (Double(mathScore) + Double(englishScore)) / 2
     }
+}
+
+for i in 1...10 {
+    var stu:student = student(name: "a\(i)", mathScore: 84, englishScore: 72)
+    print(stu)
 }
