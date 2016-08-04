913fccf89e0003f0b76ffe817261a6e3fd5d6d35
1900365 <1900365@qq.com>
2016-08-04 22:50:11 +0800

    playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct student {
+    var name:String
+    var mathScore:Int
+    var englishScore:Int
+    var averageScore:Double
+    
+    init (name: String, mathScore: Int, englishScore: Int, averageScore: Double) {
+        self.name
+    }
+}
