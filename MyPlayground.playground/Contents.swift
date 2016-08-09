e444551aa8c6c61df17b2a7dbb06bec89b18a17b
1900365 <1900365@qq.com>
2016-08-09 19:50:19 +0800

    playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,22 +1,8 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-struct student {
-    var name:String
-    var mathScore:Int
-    var englishScore:Int
-    var averageScore:Double
+let arr = ["a","b","c","d","e","f","g"]
 
-    init (name:String="No Name", mathScore:Int=0, englishScore:Int=0, averageScore:Double=0) {
-        self.name = name
-        self.mathScore = mathScore
-        self.englishScore = englishScore
-        self.averageScore = (Double(mathScore) + Double(englishScore)) / 2
-    }
-}
-
-for i in 1...10 {
-    var stu:student = student(name: "a\(i)", mathScore: 84, englishScore: 72)
-    print(stu)
+for i in 0 ..< arr.count{
+    print(arr[i])
+    print(arr[arr.startIndex.advancedBy(i)])
 }
