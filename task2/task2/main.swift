791aa529515d965bf713cea83875bbb8b3d58d63
1900365 <1900365@qq.com>
2016-08-12 18:32:19 +0800

    task2

--- task2/task2/main.swift
+++ task2/task2/main.swift
@@ -8,27 +8,39 @@
 
 import Foundation
 
-enum Direction:String{
-    case Up = "上"
-    case Right = "右"
-    case Down = "下"
-    case Left = "左"
-    
-    func getRotation()->Int{
-        switch self{
-        case .Up:
-            return 0
-        case .Right:
-            return 90
-        case .Down:
-            return 180
-        case .Left:
-            return 270
-        }
-    }
-}
+//enum Direction:String{
+//    case Up = "上"
+//    case Right = "右"
+//    case Down = "下"
+//    case Left = "左"
+//    
+//    func getRotation()->Int{
+//        switch self{
+//        case .Up:
+//            return 0
+//        case .Right:
+//            return 90
+//        case .Down:
+//            return 180
+//        case .Left:
+//            return 270
+//        }
+//    }
+//}
+//print(Direction.Right)
+//print(Direction.Right.rawValue)
+//print(Direction.Right.hashValue)
+//print(Direction.Right.getRotation())
 
-print(Direction.Right)
-print(Direction.Right.rawValue)
-print(Direction.Right.hashValue)
-print(Direction.Right.getRotation())
+var arr = [2,3,4,5,3.2]
+print(arr)
+arr[0] = 10
+print(arr)
+var arrInt = [Int]() //定义一个Int类型的空数组
+var arr1 = [NSObject]()
+arr1.append(1)
+arr1.append("bbb")
+arr1.append(3.14)
+print(arr1)
+arr1.removeAtIndex(1)
+print(arr1)
